a9671114939a829b78799652a8c53bfe02e20686
18anguyen9 <avn001@ucsd.edu>
2022-10-14 22:51:23 -0700

    add project dropdown

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -17,14 +17,14 @@ function Home() {
 
 				<div className='flex pt-2'>
 					<div className = 'pr-2'>
-						<button class="bg-slate-100	 hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded inline-flex items-center">
+						<button class="bg-slate-100 hover:bg-gray-400 text-gray-900  font-bold py-2 px-2 rounded inline-flex items-center">
 							<svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
 							<a  href={require("../../Alex_Resume.pdf")} download="Alex_Nguyen_Resume">Resume</a>
 						</button>
 					</div>
 					
 					<div className='pl-2'>
-						<button class="bg-slate-100	hover:bg-gray-400 text-gray-800 font-bold py-2 px-2 rounded inline-flex items-center">
+						<button class="bg-slate-100	hover:bg-gray-400 text-gray-900 font-bold py-2 px-2 rounded inline-flex items-center">
 							<a>Let's talk</a>
 						</button>
 					</div>
